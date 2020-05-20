6c6b9caf8eac16c8338c2a36ab8af53be7b07340
RazorProBowl <razorprobowl@gmail.com>
2020-05-20 18:11:32 -0400

    Co-authored-by: Esther Chung <e.chung059@gmail.com>

--- src/History.js
+++ src/History.js
@@ -1,23 +1,132 @@
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck'
 const React = require('react')
 
-  class History extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      }
+class History extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
     }
+    
+  this.fasts = [
+    {
+      date: "3/3/2020",
+      expTime: 5,
+      actualTime: 5,
+      status: "Success!"
+    },
+    {
+      date: "3/3/2020",
+      expTime: 5,
+      actualTime: 5,
+      status: "Success!"
+    },
+    {
+      date: "3/3/2020",
+      expTime: 5,
+      actualTime: 5,
+      status: "Success!"
+    },
+    {
+      date: "3/3/2020",
+      expTime: 4,
+      actualTime: 5,
+      status: "Success!"
+    }
+  ];
+  }
   
-    render() {
-        return (
-              <Card bg="secondary" style={{ marginTop: '10vh', width: '80rem', height: '20vh'}}>
-  <Card.Body>
-    <Card.Title style={{ fontSize: '25px'}}>History</Card.Title>
-  </Card.Body>
-</Card>
-               
-        );
-      }
-    }
+  render(){
+      return(
+    <Card bg="secondary" style={{ marginTop: '5vh', marginBottom: '-5vh', width: '80rem', height: '30vh' }}>
+      <Card.Body>
+        <Card.Title style={{ fontSize: '25px' }}>History</Card.Title>
+
+        <CardDeck>
+          {this.fasts.map( (fast) => {
+            return(
+                
+              
+              {isSuccess = (fast.expTime - fast.actualTime === 0) ? (
+                <Card bg="light" text="dark" style={{ marginTop: '2vh', width: '15rem', height: '18vh' }}>
+                    <Card.Header className="text-light bg-success" style={{ fontSize: '17px' }}> {fast.date}</Card.Header> 
+                    <Card.Body>
+                        <Card.Title style={{ fontSize: '20px' }}> {fast.time} </Card.Title>
+                        <Card.Text style={{ fontSize: '15px' }}> Success! </Card.Text>
+                    </Card.Body> 
+                </Card>
+              ) : 
+              (
+                <Card bg="light" text="dark" style={{ marginTop: '2vh', width: '15rem', height: '18vh' }}>
+                    <Card.Header className="text-light bg-danger" style={{ fontSize: '17px' }}> {fast.date}</Card.Header>
+                    <Card.Body>
+                        <Card.Title style={{ fontSize: '20px' }}> {fast.time} </Card.Title>
+                        <Card.Text style={{ fontSize: '15px' }}> Failure </Card.Text>
+                    </Card.Body>
+                </Card>
+              )
+            }
  
-export default History;
+            
+            )
+          })}
+        </CardDeck>
+
+      </Card.Body>
+    </Card>
+      );
+}
+  
+  }
+  export default History;
+
+//   render() {
+//     return (
+//       <Card bg="secondary" style={{ marginTop: '5vh', marginBottom: '-5vh', width: '80rem', height: '30vh' }}>
+//         <Card.Body>
+//           <Card.Title style={{ fontSize: '25px' }}>History</Card.Title>
+
+//           <CardDeck>
+
+//             <Card bg="light" text="dark" style={{ marginTop: '2vh', width: '15rem', height: '18vh' }}>
+//               <Card.Header className="text-light bg-success" style={{ fontSize: '17px' }}>3/3/2020</Card.Header>
+//               <Card.Body>
+//                 <Card.Title style={{ fontSize: '20px' }}> 5h / 5h </Card.Title>
+//                 <Card.Text style={{ fontSize: '15px' }}> Success! </Card.Text>
+//               </Card.Body>
+//             </Card>
+
+//             <Card bg="light" text="dark" style={{ marginTop: '2vh', width: '15rem', height: '18vh' }}>
+//               <Card.Header className="text-light bg-success" style={{ fontSize: '17px' }}>4/10/2020</Card.Header>
+//               <Card.Body>
+//                 <Card.Title style={{ fontSize: '20px' }}> 10h / 10h </Card.Title>
+//                 <Card.Text style={{ fontSize: '15px' }}> Success! </Card.Text>
+//               </Card.Body>
+//             </Card>
+
+//             <Card bg="light" text="dark" style={{ marginTop: '2vh', width: '15rem', height: '18vh' }}>
+//               <Card.Header className="text-light bg-danger" style={{ fontSize: '17px' }}>5/1/2020</Card.Header>
+//               <Card.Body>
+//                 <Card.Title style={{ fontSize: '20px' }}> 5h 30m / 10h </Card.Title>
+//                 <Card.Text style={{ fontSize: '15px' }}> Not a Success </Card.Text>
+//               </Card.Body>
+//             </Card>
+
+
+//             <Card bg="light" text="dark" style={{ marginTop: '2vh', width: '15rem', height: '18vh' }}>
+//               <Card.Header className="text-light bg-success" style={{ fontSize: '17px' }}>5/11/2020</Card.Header>
+//               <Card.Body>
+//                 <Card.Title style={{ fontSize: '20px' }}> 2h 30m / 10h </Card.Title>
+//                 <Card.Text style={{ fontSize: '15px' }}> Success! </Card.Text>
+//               </Card.Body>
+//             </Card>
+
+//           </CardDeck>
+
+//         </Card.Body>
+//       </Card>
+
+//     );
+//   }
+// }
+
--- src/Timer.js
+++ src/Timer.js
@@ -17,78 +17,163 @@ class Time extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ fontSize: 100, marginLeft: 100 }}>{this.props.value}:{this.props.seconds}</h1>
+        <h1 style={{ fontSize: 100, marginLeft: 100 }}>{this.props.hours}:{this.props.value}:{this.props.seconds}</h1>
       </div>
     );
   }
 }
 
+class StartButton extends React.Component {
+    render() {
+      return (
+          <button className="btn btn-lg btn-success" style={{ marginLeft: '75px' }} disabled={!this.props.value} onClick={this.props.startCountDown}>Start</button>
+      );
+    }
+  }
+
 class Timer extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state = {
       seconds: '00',
-      value: '',
-      isClicked: false
+      value: '00',
+      hours: '00',
+      start: '00',
+      isClicked: false,
+      isCustom: false
     }
-    //this.secondsRemaining;
-    //this.intervalHandle;
+    this.secondsRemaining = 0;
+    this.intervalHandle = 0;
     this.handleChange = this.handleChange.bind(this);
     this.startCountDown = this.startCountDown.bind(this);
     this.tick = this.tick.bind(this);
+    this.eighteenSix = this.eighteenSix.bind(this);
+    this.sixteenEight = this.sixteenEight.bind(this);
+    this.stop = this.stop.bind(this);
+    this.customTime = this.customTime.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   handleChange(event) {
     this.setState({
-      value: event.target.value
+      hours: event.target.value,
+      value: '00',
+      seconds: '00',
+      start: event.target.value
     })
   }
 
   tick() {
-    var min = Math.floor(this.secondsRemaining / 60);
-    var sec = this.secondsRemaining - (min * 60);
-
-    this.setState({
-      value: min,
-      seconds: sec,
-    })
-
-    if (sec < 10) {
-      this.setState({
-        seconds: "0" + this.state.seconds,
-      })
-
-    }
-
-    if (min < 10) {
-      this.setState({
-        value: "0" + min,
-      })
-
-    }
-
-    if (min === 0 & sec === 0) {
-      clearInterval(this.intervalHandle);
-    }
-
-
-    this.secondsRemaining--
+        var hour = Math.floor((this.secondsRemaining / 60) / 60);
+        var min = Math.floor((this.secondsRemaining / 60) % 60);
+        var sec = this.secondsRemaining - (min * 60) - (hour * 3600);
+    
+        this.setState({
+          hours: hour,
+          value: min,
+          seconds: sec,
+        })
+    
+        if (sec < 10) {
+          this.setState({
+            seconds: "0" + this.state.seconds,
+          })
+    
+        }
+    
+        if (min < 10) {
+          this.setState({
+            value: "0" + this.state.value,
+          })
+    
+        }
+    
+        if (hour < 10) {
+            this.setState({
+              hours: "0" + this.state.hours,
+            })
+      
+          }
+    
+        if (min === 0 & sec === 0 & hour === 0) {
+          clearInterval(this.intervalHandle);
+        }
+    
+    
+        this.secondsRemaining--
   }
 
   startCountDown() {
     this.intervalHandle = setInterval(this.tick, 1000);
-    let time = this.state.value;
-    this.secondsRemaining = time * 60;
+    if(!this.state.isClicked){
+          this.secondsRemaining = ((this.state.hours * 3600) + (this.state.value * 60) + this.state.seconds)/100;
+    }
+    else{
+        this.secondsRemaining = ((this.state.hours * 3600) + (this.state.value * 60) + this.state.seconds);
+    }
+    
     this.setState({
-      isClicked: true
+      isClicked: true,
+      isCustom: false,
     })
+  }
+
+  eighteenSix() {
+    this.setState({
+        hours: '18',
+        value: '00',
+        seconds: '00',
+        start: '18'
+      })
+  }
+
+  sixteenEight() {
+    this.setState({
+        hours: '16',
+        value: '00',
+        seconds: '00',
+        start: '16'
+      })
+  }
+
+  stop(){
+    this.setState({
+        isClicked: false
+      })
+      clearInterval(this.intervalHandle);
+  }
+
+  customTime(){
+    this.setState({
+        isCustom: true
+      })
+  }
+
+  reset(){
+    this.setState({
+        hours: this.state.start,
+        value: '00',
+        seconds: '00',
+      })
+      this.secondsRemaining = this.state.start * 60 * 60;
+    clearInterval(this.intervalHandle);
+    this.intervalHandle = setInterval(this.tick, 1000);
   }
 
   render() {
     const clicked = this.state.isClicked;
     if (clicked) {
       return (
-        <Time value={this.state.value} seconds={this.state.seconds} />
+        <Card bg="secondary" style={{ width: '80rem', height: '50vh' }}>
+        <Card.Body>
+          <Card.Title style={{ fontSize: '85px' }}>Intermittent Fasting Timer</Card.Title>
+          <p class="text-info" style={{ fontSize: '24px' }}>Pick a fasting time below and watch the clock count down the time until your next meal.</p>
+          <Time hours={this.state.hours} value={this.state.value} seconds={this.state.seconds} />
+          <button className="btn btn-lg btn-primary" style={{ marginLeft: '25px' }} onClick={this.reset}>Reset</button>
+          <button className="btn btn-lg btn-danger" style={{ marginLeft: '75px' }} onClick={this.stop}>Stop</button>
+        </Card.Body>
+      </Card>
       );
     } else {
       return (
@@ -96,11 +181,13 @@ class Timer extends React.Component {
           <Card.Body>
             <Card.Title style={{ fontSize: '85px' }}>Intermittent Fasting Timer</Card.Title>
             <p class="text-info" style={{ fontSize: '24px' }}>Pick a fasting time below and watch the clock count down the time until your next meal.</p>
-            <Time value={this.state.value} seconds={this.state.seconds} />
-            <button className="btn btn-lg btn-primary" onClick={this.props.startCountDown}>16:8</button>
-            <button className="btn btn-lg btn-primary" style={{ marginLeft: '25px' }} onClick={this.props.startCountDown}>18:6</button>
-            <button className="btn btn-lg btn-primary" style={{ marginLeft: '25px' }} onClick={this.props.startCountDown}>Custom</button>
-            <button className="btn btn-lg btn-success" style={{ marginLeft: '75px' }} onClick={this.props.startCountDown}>Start</button>
+            <Time hours={this.state.hours} value={this.state.value} seconds={this.state.seconds} />
+            <button className="btn btn-lg btn-primary" onClick={this.sixteenEight}>16:8</button>
+            <button className="btn btn-lg btn-primary" style={{ marginLeft: '25px' }} onClick={this.eighteenSix}>18:6</button>
+            <button className="btn btn-lg btn-primary" style={{ marginLeft: '25px' }} onClick={this.customTime}>Custom</button>
+            <StartButton startCountDown={this.startCountDown} value={this.state.value} />
+            {this.state.isCustom ? <TimerInput value={this.state.hours} handleChange={this.handleChange} /> : null}
+            
           </Card.Body>
         </Card>
 
@@ -108,8 +195,8 @@ class Timer extends React.Component {
     }
   }
 }
-/*
-<TimerInput value={this.state.value} handleChange={this.handleChange} />
-*/
+
+
+
 
 export default Timer;
